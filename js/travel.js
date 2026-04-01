57955479d22406a57a7033f869ff39ce3530d14d
Bentropy-AI <beneben_uk@hotmail.com>
2026-04-01 12:29:20 +0100

    Fix Travel Tracker nav link to point to root directory instead of index.html

--- js/travel.js
+++ js/travel.js
@@ -140,7 +140,7 @@ function renderNav(activeKey) {
     analyticsHtml += '<li><a href="' + a.href + '"' + aCl + '>' + a.label + '</a></li>';
   }
 
-  var html = '<a class="nav-brand" href="' + root + 'index.html">&#9992; Travel Tracker</a>' +
+  var html = '<a class="nav-brand" href="' + root + '">&#9992; Travel Tracker</a>' +
     '<ul class="nav-menu">' +
       '<li class="nav-dropdown"><span class="nav-dropdown-label">Travel Lists <span class="caret">&#9660;</span></span><ul class="nav-submenu">' + listsHtml + '</ul></li>' +
       '<li class="nav-dropdown"><span class="nav-dropdown-label">Travellers <span class="caret">&#9660;</span></span><ul class="nav-submenu">' + travHtml + '</ul></li>' +
